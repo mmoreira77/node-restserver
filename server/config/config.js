e81fd86d257e0d1064418e107f2261d3b16694eb
Melvin Gonzalez <melvin@intranet.esen.edu.sv>
2019-07-17 14:04:53 -0600

    Definicion de token

--- server/config/config.js
+++ server/config/config.js
@@ -10,6 +10,18 @@ process.env.PORT = process.env.PORT || 3000
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //----------------------------------------------------
+// Vencimiento de token
+//----------------------------------------------------
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+//----------------------------------------------------
+// SEED de autenticación
+//----------------------------------------------------
+
+process.env.SEED = process.env.SEED ||  'este-es-el-seed-desarrollo'
+
+//----------------------------------------------------
 // BASES DE DATOS
 //----------------------------------------------------
 
