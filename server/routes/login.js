1561ae5896d0d8c0220e5038843b6b6615458658
Melvin Gonzalez <melvin@intranet.esen.edu.sv>
2019-07-26 16:49:07 -0600

    Prueba con ruta google bug

--- server/routes/login.js
+++ server/routes/login.js
@@ -73,6 +73,10 @@ async function verify(token) {
 app.post('/google', async (req, res) => {
     let token = req.body.idtoken;
     console.log(token);
+    return res.status(500).json({
+        ok: 'true',
+        toke
+    });
     //let googleUser = await verify(token);
     //     .catch(e => {
     //         return res.status(403).json({
