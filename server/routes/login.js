4b463ce2162bdaa72056cb818f850fb38cd48caf
Melvin Gonzalez <melvin@intranet.esen.edu.sv>
2019-07-26 16:35:14 -0600

    Debug de ruta /google

--- server/routes/login.js
+++ server/routes/login.js
@@ -72,71 +72,72 @@ async function verify(token) {
 
 app.post('/google', async (req, res) => {
     let token = req.body.idtoken;
-    let googleUser = await verify(token)
-        .catch(e => {
-            return res.status(403).json({
-                ok: false,
-                err: e
-            });
-        });
+    console.log(token);
+    //let googleUser = await verify(token);
+    //     .catch(e => {
+    //         return res.status(403).json({
+    //             ok: false,
+    //             err: e
+    //         });
+    //     });
 
-    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                err
-            })
-        }
+    // Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+    //     if (err) {
+    //         return res.status(500).json({
+    //             ok: false,
+    //             err
+    //         })
+    //     }
 
-        if (usuarioDB) {
-            if (usuarioDB.google === false) {
-                return res.status(400).json({
-                    ok: false,
-                    err: {
-                        message: 'Debe usar su autenticación normal'
-                    }
-                })
-            } else {
-                let token = jwt.sign({
-                    usuario: usuarioDB
-                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+    //     if (usuarioDB) {
+    //         if (usuarioDB.google === false) {
+    //             return res.status(400).json({
+    //                 ok: false,
+    //                 err: {
+    //                     message: 'Debe usar su autenticación normal'
+    //                 }
+    //             })
+    //         } else {
+    //             let token = jwt.sign({
+    //                 usuario: usuarioDB
+    //             }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
 
-                return res.json({
-                    ok: true,
-                    usuario: usuarioDB,
-                    token
-                });
-            }
-        } else {
-            //Usuario no existe en la base de datos
-            let usuario = new Usuario();
-            usuario.nombre = googleUser.nombre;
-            usuario.email = googleUser.email;
-            usuario.img = googleUser.img;
-            usuario.google = true;
-            usuario.password = ':)';
+    //             return res.json({
+    //                 ok: true,
+    //                 usuario: usuarioDB,
+    //                 token
+    //             });
+    //         }
+    //     } else {
+    //         //Usuario no existe en la base de datos
+    //         let usuario = new Usuario();
+    //         usuario.nombre = googleUser.nombre;
+    //         usuario.email = googleUser.email;
+    //         usuario.img = googleUser.img;
+    //         usuario.google = true;
+    //         usuario.password = ':)';
 
-            usuario.save((err, usuarioDB) => {
-                if (err) {
-                    return res.status(500).json({
-                        ok: false,
-                        err
-                    })
-                }
+    //         usuario.save((err, usuarioDB) => {
+    //             if (err) {
+    //                 return res.status(500).json({
+    //                     ok: false,
+    //                     err
+    //                 })
+    //             }
 
-                let token = jwt.sign({
-                    usuario: usuarioDB
-                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+    //             let token = jwt.sign({
+    //                 usuario: usuarioDB
+    //             }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
 
-                return res.json({
-                    ok: true,
-                    usuario: usuarioDB,
-                    token
-                });
+    //             return res.json({
+    //                 ok: true,
+    //                 usuario: usuarioDB,
+    //                 token
+    //             });
 
-            });
-        }
-    });
+    //         });
+    //     }
+    // });
 });
 
 module.exports = app;
