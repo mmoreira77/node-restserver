22d1ab67fee809e5a0fc8d3dea8d01511c2c620f
Melvin Gonzalez <melvin@intranet.esen.edu.sv>
2019-07-29 13:08:15 -0600

    Debug usuario de google arreglado el problema era la client_id tiena una caracterer de más

--- server/routes/login.js
+++ server/routes/login.js
@@ -83,69 +83,70 @@ app.post('/google', async (req, res) => {
             });
         });
 
-    return res.json({
-        ok: true,        
-        googleUser
-    });
-
-
-    // Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
-    //     if (err) {
-    //         return res.status(500).json({
-    //             ok: false,
-    //             err
-    //         })
-    //     }
-
-    //     if (usuarioDB) {
-    //         if (usuarioDB.google === false) {
-    //             return res.status(400).json({
-    //                 ok: false,
-    //                 err: {
-    //                     message: 'Debe usar su autenticación normal'
-    //                 }
-    //             })
-    //         } else {
-    //             let token = jwt.sign({
-    //                 usuario: usuarioDB
-    //             }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
-
-    //             return res.json({
-    //                 ok: true,
-    //                 usuario: usuarioDB,
-    //                 token
-    //             });
-    //         }
-    //     } else {
-    //         //Usuario no existe en la base de datos
-    //         let usuario = new Usuario();
-    //         usuario.nombre = googleUser.nombre;
-    //         usuario.email = googleUser.email;
-    //         usuario.img = googleUser.img;
-    //         usuario.google = true;
-    //         usuario.password = ':)';
-
-    //         usuario.save((err, usuarioDB) => {
-    //             if (err) {
-    //                 return res.status(500).json({
-    //                     ok: false,
-    //                     err
-    //                 })
-    //             }
-
-    //             let token = jwt.sign({
-    //                 usuario: usuarioDB
-    //             }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
-
-    //             return res.json({
-    //                 ok: true,
-    //                 usuario: usuarioDB,
-    //                 token
-    //             });
-
-    //         });
-    //     }
+    // return res.json({
+    //     ok: true,
+    //     googleUser,
+    //     token
     // });
+
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+
+        if (usuarioDB) {
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe usar su autenticación normal'
+                    }
+                })
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+            }
+        } else {
+            //Usuario no existe en la base de datos
+            let usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioDB) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    })
+                }
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+                return res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                });
+
+            });
+        }
+    });
 });
 
 module.exports = app;
