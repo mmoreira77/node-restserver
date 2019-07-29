fc8e42f5fc8db54c43c751d11d3ad9ca2d49e3c8
Melvin Gonzalez <melvin@intranet.esen.edu.sv>
2019-07-29 12:49:12 -0600

    Debug

--- server/routes/login.js
+++ server/routes/login.js
@@ -52,22 +52,23 @@ app.post('/login', (req, res) => {
 });
 
 async function verify(token) {
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-        // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-    });
-    const payload = ticket.getPayload();
-    // console.log(payload.name);
-    // console.log(payload.email);
-    // console.log(payload.picture);
-    return {
-        nombre: payload.name,
-        email: payload.email,
-        img: payload.picture,
-        google: true
-    }
+    console.log(process.env.CLIENT_ID);
+    // const ticket = await client.verifyIdToken({
+    //     idToken: token,
+    //     audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+    //     // Or, if multiple clients access the backend:
+    //     //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    // });
+    // const payload = ticket.getPayload();
+    // // console.log(payload.name);
+    // // console.log(payload.email);
+    // // console.log(payload.picture);
+    // return {
+    //     nombre: payload.name,
+    //     email: payload.email,
+    //     img: payload.picture,
+    //     google: true
+    // }
 }
 
 app.post('/google', async (req, res) => {
